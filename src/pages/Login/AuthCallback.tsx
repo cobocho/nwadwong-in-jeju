659393c1b2277f45baec61d2551874571e0839fc
cobocho <rlaalsrb1111@naver.com>
2023-07-06 10:47:11 +0900

    feat: 카카오 로그인 로직 구현

---
+++ src/pages/Login/AuthCallback.tsx
@@ -0,0 +1,64 @@
+import { useNavigate } from 'react-router-dom';
+const { VITE_APP_KEY, VITE_REDIRECT_URI } = import.meta.env;
+
+const AuthCallback = () => {
+  const params = new URL(document.location.toString()).searchParams;
+  const CODE = params.get('code');
+  const navigation = useNavigate();
+
+  async function getToken() {
+    const CLIENT_ID = VITE_APP_KEY;
+    const REDIRECT_URI = VITE_REDIRECT_URI;
+    const res = await fetch(
+      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&code=${CODE}`,
+      {
+        headers: { 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8' },
+      }
+    );
+    const { access_token } = await res.json();
+
+    if (access_token) {
+      const res = await fetch(`https://kapi.kakao.com/v2/user/me`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      });
+      const { id } = await res.json();
+      const request = JSON.stringify({
+        id: String(id),
+      });
+      const { token } = await (
+        await fetch(`/api/oauth-redirect`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: request,
+        })
+      ).json();
+
+      const data = await (
+        await fetch(`/api/oauth-test`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+          },
+        })
+      ).json();
+
+      navigation('/login');
+    } else {
+      alert('유효하지 않은 카카오 ID 입니다!');
+      navigation('/login');
+    }
+  }
+
+  getToken();
+
+  return <></>;
+};
+
+export default AuthCallback;
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+const { VITE_APP_KEY, VITE_REDIRECT_URI } = import.meta.env;
 
 export default function Login() {
-  return <div>Login</div>;
+  function loginWithKakao() {
+    const CLIENT_ID = VITE_APP_KEY;
+    const REDIRECT_URI = VITE_REDIRECT_URI;
+    const AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    window.location.href = AUTH_URL;
+  }
+
+  return <button onClick={loginWithKakao}>카카오 로그인</button>;
 }
