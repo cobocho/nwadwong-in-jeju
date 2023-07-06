be78e7f1975521ad0402736d8e74e7a485399046
cobocho <rlaalsrb1111@naver.com>
2023-07-06 19:56:31 +0900

    add: 로고 애니메이션 구현

--- src/pages/Login/KakaoLoginButton.tsx
+++ src/pages/Login/KakaoLoginButton.tsx
@@ -29,9 +29,6 @@ const KakaoLoginButton = () => {
 };
 
 const Container = styled.button`
-  position: absolute;
-  bottom: 0;
-  left: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
