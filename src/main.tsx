5e688405a743f68c2d1be6abd2c6c0fa4cb132a0
Seoyun Kang <seoyunfleuve@gmail.com>
2023-07-06 10:22:52 +0900

    Add : styled-components 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import Router from './Router';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Router from "./Router";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyles from "./styles/GlobalStyles";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
 root.render(
   <RecoilRoot>
-    <RouterProvider router={Router} />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={Router} />
+    </ThemeProvider>
   </RecoilRoot>
 );
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset};
+
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: white;
+    }
+
+    ol, ul {
+	list-style: none;
+    }
+
+    a {
+	text-decoration: none;
+	color: inherit;
+
+        &:hover {
+            text-decoration: none;
+        }
+        
+        &:active {
+            text-decoration: none;
+        }
+            
+        &:visited {
+            text-decoration: none;
+        }
+            
+        &:link {
+            text-decoration: none;
+        }
+    }`;
+
+export default GlobalStyles;
